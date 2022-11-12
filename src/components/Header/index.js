41f4669ef5ef81b07e375f19f8b129029cc91244
thaling <thaling@verizon.net>
2022-11-12 10:26:03 -0700

    styling updates

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,26 +2,19 @@ import React from "react";
 
 const Header = () => {
   return (
-      <div>
-    <nav>
-      <ul>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-        <li>
-          <a href="#resume">Resume</a>
-        </li>
-      </ul>
-    </nav>
     <div>
+      <nav id="navbar">
+        <a href="#about">About</a>
+
+        <a href="#portfolio">Portfolio</a>
+
+        <a href="#contact">Contact</a>
+
+        <a href="#resume">Resume</a>
+      </nav>
+      <div>
         <h2>Tori Haling</h2>
-    </div>
+      </div>
     </div>
   );
 };
